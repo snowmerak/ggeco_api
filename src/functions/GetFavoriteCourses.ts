a6cc97c988f7ee8139ba5112c60bb2b01e921970
snowmerak <snowmerak@outlook.com>
2023-12-10 22:43:41 +0900

    Update GetFavoriteCourses.ts

--- src/functions/GetFavoriteCourses.ts
+++ src/functions/GetFavoriteCourses.ts
@@ -1,6 +1,7 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { getAndVerifyAccessToken, Payload } from "../auth/token";
 import { PrismaClient } from "@prisma/client";
+import { CourseData, ErrorCode, getCourseData } from "../course/course";
 
 export async function GetFavoriteCourses(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     let token = new Payload();
@@ -24,11 +25,36 @@ export async function GetFavoriteCourses(request: HttpRequest, context: Invocati
         },
     });
 
-    return { body: `Hello, ${name}!` };
+    let courses = new Array<CourseData>();
+    for (const favorite_course of favorite_courses) {
+        const course_data = await getCourseData(favorite_course.course_id);
+
+        if (course_data instanceof ErrorCode) {
+            return {
+                status: course_data.code,
+                body: JSON.stringify({
+                    error: course_data.message,
+                }),
+            }
+        }
+
+        if (course_data.course.is_public == 0) {
+            continue;
+        }
+
+        course_data.is_favorite = true;
+
+        courses.push(course_data);
+    }
+
+    return {
+        status: 200,
+        body: JSON.stringify(courses),
+    };
 };
 
 app.http('GetFavoriteCourses', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
+    methods: ['GET'],
+    authLevel: 'function',
     handler: GetFavoriteCourses
 });
