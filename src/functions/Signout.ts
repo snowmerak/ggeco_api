00b566fdaf241c725e0bccd8af64b66e614ef03b
snowmerak <snowmerak@outlook.com>
2023-12-11 21:54:53 +0900

    make signout

---
+++ src/functions/Signout.ts
@@ -0,0 +1,37 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { getAndVerifyAccessToken, Payload } from "../auth/token";
+import { PrismaClient } from "@prisma/client";
+
+export async function Signout(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    let token = new Payload();
+    try {
+        const data = getAndVerifyAccessToken(request.headers);
+        token = data;
+    } catch (e) {
+        return {
+            status: 401,
+            body: JSON.stringify({
+                error: e.message
+            })
+        }
+    }
+
+    const db = new PrismaClient();
+
+    await db.users.update({
+        where: {
+            id: token.id,
+        },
+        data: {
+            removed_at: new Date(),
+        }
+    });
+
+    return { status: 200 };
+};
+
+app.http('Signout', {
+    methods: ['PATCH'],
+    authLevel: 'function',
+    handler: Signout
+});
